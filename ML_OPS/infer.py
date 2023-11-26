c7f7fe02fa658c99636fa028ed734cfbe1dc943c
Daniil Berezin <berezin.da@phystech.edu>
2023-11-27 01:07:39 +0300

    NN model CS-GO

--- ML_OPS/infer.py
+++ ML_OPS/infer.py
@@ -1,55 +1,32 @@
-import joblib
 import numpy as np
 import pandas as pd
-import requests
-from catboost import CatBoostClassifier
-from sklearn.metrics import recall_score
+import torch
 from sklearn.model_selection import train_test_split
 
-try:
-    f = open("dataset.csv")
-    f.close()
-except FileNotFoundError:
-    file_id = "1HJTDfP6njwgsToIrSSPBNNRXvh9QmX_R"
-    url = f"https://drive.google.com/uc?export=download&id={file_id}"
-    r = requests.get(url)
-    while r.status_code != 200:
-        r = requests.get(url)
-    with open("dataset.csv", "wb") as f:
-        f.write(r.content)
+from ML_OPS.NN_model import my_model
 
-df = pd.read_csv("dataset.csv")
-target = "Dx:Cancer"
-df = df.drop_duplicates()
-
-for name in df.columns:
-    mean = np.array(df[f"{name}"] != "?").mean()
-    df[name] = df[name].replace({"?": mean})
-
-X = df.drop(columns=target)
-y = df[target]
-
-X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.33, random_state=42, stratify=y, shuffle=True
+path = "../dataset.csv"
+full_data = pd.read_csv(path)
+X = full_data.drop(columns=["round_winner"])
+y = full_data["round_winner"]
+_, X_test, _, y_test = train_test_split(
+    X, y, random_state=42, train_size=0.85, stratify=y
 )
 
+BATCH_SIZE = 1024
+NUM_FEATURES = X.shape[-1]
+NUM_CLASSES = len(np.unique(y))
 
-def score(model, X_test, y_test):
-    y_pred = model.predict(X_test)
-    return recall_score(y_test, y_pred, average="macro"), y_pred
-
-
-scaler_filename = "scaler"
-scaler = joblib.load(scaler_filename)
-
-
-X_test_scal = scaler.transform(X_test)
-
-model = CatBoostClassifier()
-model.load_model("model")
-
-Score, y_pred = score(model, X_test, y_test)
-print("recall_score =", Score)
-
-ans = pd.DataFrame({"predict": y_pred})
-ans.to_csv("predict.csv")
+X_test, y_test = np.array(X_test, dtype=np.float32), y_test.values
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model = my_model(NUM_FEATURES, NUM_CLASSES).to(device)
+path = "Model.pth"
+model.load(path)
+model.eval()
+outputs = model(torch.from_numpy(X_test).float().to(device))
+_, predict = torch.max(outputs.data, 1)
+labels = torch.from_numpy(y_test).float().to(device)
+correct = (predict == labels).sum().item()
+total = labels.size(0)
+acc = correct / total
+print(f"accuracy: {acc:.3f}")
